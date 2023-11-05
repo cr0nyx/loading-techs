1bd2d60df82c2687566d438a9ae44f79be56b0cc
cr0nyx <cr0nyx@localhost>
2023-11-05 13:53:06 -0800

    init

---
+++ Loader-Techs/DLL Injectors/Classic-DLL-Injector/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.IO;
+
+namespace Classic_DLL_Injector
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, uint processId);
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [Flags]
+        public enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+        static void Main(string[] args)
+        {
+            var dll_path = "";
+            uint pid = 1337;
+            bool inheritHandle = false;
+
+            // processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, DWORD(atoi(argv[1])));
+            IntPtr processHandle = OpenProcess((uint)ProcessAccessFlags.All, inheritHandle, pid);
+            Console.WriteLine("[+] Handle for process {0} - success", pid);
+
+            // remoteBuffer = VirtualAllocEx(processHandle, NULL, sizeof dllPath, MEM_COMMIT, PAGE_READWRITE);
+            var dll_bytes = File.ReadAllBytes(dll_path);
+            var addr = VirtualAllocEx(processHandle, IntPtr.Zero, (uint)dll_bytes.Length, (uint)AllocationType.Commit, (uint)MemoryProtection.ReadWrite);
+            Console.WriteLine("[+] Allocate space in process {0} - success", pid);
+
+            // WriteProcessMemory(processHandle, remoteBuffer, (LPVOID)dllPath, sizeof dllPath, NULL);
+            WriteProcessMemory(processHandle, addr, dll_bytes, dll_bytes.Length, out IntPtr lpNumberOfBytesWritten);
+            Console.WriteLine("[+] Copying DLL - success");
+
+            // PTHREAD_START_ROUTINE threatStartRoutineAddress = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(TEXT("Kernel32")), "LoadLibraryW");
+
+
+            // CreateRemoteThread(processHandle, NULL, 0, threatStartRoutineAddress, remoteBuffer, 0, NULL);
+
+            // CloseHandle(processHandle);
+        }
+    }
+}
